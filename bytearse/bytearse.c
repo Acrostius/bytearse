67cef4a41bf8a7879a849c19e4f2775f41147a23
Acrostius <google@staff.8shield.net>
2024-02-19 22:16:09 -0300

    Fixed a _CrtlIsValidHeapPointer bug because of faulty array accesses. Implemented new constant related functionality.

--- bytearse/bytearse.c
+++ bytearse/bytearse.c
@@ -4,13 +4,16 @@
 
 int main(int argc, const char* argv[])
 {
+    // Initialize default chunk values to 0.
     Chunk chunk;
     initChunk(&chunk);
-
+    
+    // Add constant value 1.2f to constant pool. Needs to have precision lowered.
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
+    // -- ONLY FOR TESTING PURPOSES (REMOVE LATER)
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
